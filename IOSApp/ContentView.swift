df954bf8588674b4a1712e5cdc1eaf6b2946251c
Wenzel Joubert <wenzel.joubert@gmail.com>
2021-05-22 14:05:28 +0800

    Initial Commit

---
+++ IOSApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOSApp
+//
+//  Created by wenzel Joubert on 22/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IOSApp/IOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSAppApp.swift
+//  IOSApp
+//
+//  Created by wenzel Joubert on 22/5/21.
+//
+
+import SwiftUI
+
+@main
+struct IOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
